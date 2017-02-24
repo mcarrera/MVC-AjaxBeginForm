e9edb5cd05d0bc7370749df6c3333c97dd7f1b00
MC <mcarrera00@gmail.com>
2017-02-24 14:57:29 -0600

    BundleConfig

---
+++ AjaxBeginFormwithMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace AjaxBeginFormwithMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/toastr.css"));
+            bundles.Add(new ScriptBundle("~/bundles/toastr").Include("~/Scripts/toastr.min.js"));
+        }
+    }
+}
